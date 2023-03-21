0040f077dd0cb949eadecc996c79782c989a7ddb
Mobolaji Kuti <mobolajiaokuti@gmail.com>
2023-03-21 02:46:32 +0000

    populate with questions.js file with code-related questions

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "How would you create an unordered list in HTML?",
+        choices: ["<b>","<href>", "<ol>", "<ul>"],
+        answer: "<ul>"
+    },
+    {
+        title: "How many sizes of headers are available in HTML (defualt)?",
+        choices: ["5", "1", "3", "6"],
+        answer: "6"
+    },
+    {
+        title: "If we want to wrap a block of text around an image, which CSS property should we use?",
+        choices: ["wrap", "push", "float", "align"],
+        answer: "float"
+    },
+    {
+        title: "How do you comment in CSS?",
+        choices: ["/* a comment */", "<' a comment '>", "// a comment //", "/ a comment /"],
+        answer: "/* a comment */"
+    },
+    {
+        title: "Which of the following methods can be used to display data using Javacript?",
+        choices: ["document.write()", "console.log()", "window.alert()", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "Which of the following is NOT a JavaScript framework?",
+        choices: ["Node", "React", "Meteor", "View"],
+        answer: "View"
+    },
+];
